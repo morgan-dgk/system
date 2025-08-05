679904634cfb87e711ae0048fed300964e0ac127
morgan-dgk <mdgk95@proton.me>
2025-08-05 19:34:24 +1000

    Add safeeyes

--- home/niri/waybar.mod.nix
+++ home/niri/waybar.mod.nix
@@ -3,4 +3,14 @@
   programs.waybar.settings = {
     main = {modules-right = ["clock"];};
   };
+
+  services.safeeyes.enable = true;
+  services.snixembed = {
+    enable = true;
+
+    beforeUnits = [
+      # https://github.com/slgobinath/SafeEyes/wiki/How-to-install-backend-for-Safe-Eyes-tray-icon
+      "safeeyes.service"
+    ];
+  };
 }
