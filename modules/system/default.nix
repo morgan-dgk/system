af24b80a9302d6bd722859204f373649ed1893df
morgan-dgk <mdgk95@proton.me>
2025-08-10 15:04:01 +1000

    Add steam

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -14,7 +14,25 @@
   };
   programs.zsh.enable = true;
 
+  programs.gamescope = {
+    enable = true;
+    capSysNice = true;
+  };
+
+  programs.steam = {
+    enable = true;
+    gamescopeSession.enable = true;
+    extraPackages = with pkgs; [
+      gamescope
+    ];
+    extraCompatPackages = with pkgs; [
+      proton-ge-bin
+    ];
+  };
+
   users.defaultUserShell = pkgs.zsh;
+
+  hardware.xpadneo.enable = true;
 
   environment.systemPackages = with pkgs; [
     curl
