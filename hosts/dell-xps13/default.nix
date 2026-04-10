73e614da19852f57ae17e30424bbc4fb3765d06d
morgan-dgk <mdgk95@proton.me>
2026-04-10 17:17:00 +1000

    Add mullvad

--- hosts/dell-xps13/default.nix
+++ hosts/dell-xps13/default.nix
@@ -53,6 +53,8 @@
 
   services.thermald.enable = true;
 
+  services.mullvad-vpn.enable = true;
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
